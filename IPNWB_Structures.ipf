AllenInstitute/IPNWB

commit bfdb939c8636ebb00efba85b5397750e254c4c26

Neurodata Without Borders 2.0.1
This Updates the IPNWB module to specifications of NWB in version 2.0.1.

Note:

- Remove help entry for VectorIndex
- Remove conversion and resolution
- Remove description, comment, stimulus_description, sweep_number

IPNWB_Structures.ipf
@@ -99,12 +99,13 @@ End
 
 /// @brief Structure to hold all properties of the NWB file directly below `/general/subject`
 Structure SubjectInfo
-	string subject_id
+	string age
+	string date_of_birth // isodatetime
 	string description
-	string species
 	string genotype
 	string sex
-	string age
+	string species
+	string subject_id
 	string weight
 EndStructure
 
@@ -112,12 +113,13 @@ EndStructure
 threadsafe Function InitSubjectInfo(si)
 	STRUCT SubjectInfo &si
 
-	si.subject_id  = PLACEHOLDER
-	si.description = PLACEHOLDER
-	si.species     = PLACEHOLDER
+	si.age         = PLACEHOLDER
+	si.date_of_birth = GetISO8601TimeStamp()
+	si.description = "Description of subject and where subject came from (e.g., breeder, if animal)."
 	si.genotype    = PLACEHOLDER
 	si.sex         = PLACEHOLDER
-	si.age         = PLACEHOLDER
+	si.species     = PLACEHOLDER
+	si.subject_id  = PLACEHOLDER
 	si.weight      = PLACEHOLDER
 End
 
@@ -251,7 +253,6 @@ threadsafe Function InitVectorData(vd)
 End
 
 Structure VectorIndex
-	string help
 	string namespace
 	string neurodata_type
 	STRUCT VectorData target
@@ -260,7 +261,6 @@ EndStructure
 threadsafe Function InitVectorIndex(vi)
 	STRUCT VectorIndex &vi
 
-	vi.help = "indexes into a list of values for a list of elements"
 	vi.namespace = "core"
 	vi.neurodata_type = "VectorIndex"
 End

IPNWB_Constants.ipf
@@ -12,7 +12,7 @@
 StrConstant PLACEHOLDER = "PLACEHOLDER"
 
 static StrConstant NWB_VERSION_V1 = "NWB-1.0.5"
-static StrConstant NWB_VERSION_V2 = "NWB-2.0b"
+static StrConstant NWB_VERSION_V2 = "NWB-2.0.1"
 Constant NWB_VERSION_LATEST = 2
 
 /// @name HDF5 file paths
@@ -168,7 +168,7 @@ Constant REGION_REFERENCE = 0x2
 
 /// @name Constants for NWB:N Core Specifications
 /// @{
-StrConstant NWB_CORE_VERSION = "2.0b"
+StrConstant NWB_CORE_VERSION = "2.0.1"
 StrConstant NWB_SPEC_NAMESPACE = "nwb.namespace"
 StrConstant NWB_SPEC_NAMES = "nwb.base;nwb.behavior;nwb.ecephys;nwb.epoch;nwb.file;nwb.icephys;nwb.image;nwb.misc;nwb.ogen;nwb.ophys;nwb.retinotopy;"
 /// @}

IPNWB_Reader.ipf
@@ -248,7 +248,7 @@ threadsafe Function LoadSweepNumber(locationID, channel, version)
 		LoadSourceAttribute(locationID, channel, params)
 		return params.sweep
 	elseif(version == NWB_VERSION_LATEST)
-		return ReadAttributeAsNumber(locationID, channel, "sweep_number")
+		return NaN
 	endif
 End
 
@@ -496,4 +496,32 @@ threadsafe Function/S ReadNeuroDataType(fileID, name)
 	endif
 
 	return neurodata_type
+End
+
+/// @brief Return the two SweepTable data columns `sweep_number` and `series`
+///
+/// @todo Allow Executions for files with missing SweepTable entry using @ref
+/// LoadSweepNumber
+///
+/// @param locationID  HDF5 identifier
+/// @param version      major NWB version
+///
+/// @return sweep_number and path to TimeSeries as waves
+Function [WAVE/Z sweep_number, WAVE/Z/T series] LoadSweepTable(variable locationID, variable version)
+
+	string path
+	variable groupID
+
+	ASSERT_TS(version == 2, "SweepTable is only available for NWB version 2")
+	sprintf path, "%s/%s", NWB_INTRACELLULAR_EPHYS, "sweep_table"
+
+	if(IPNWB#H5_GroupExists(locationID, path, groupID = groupID))
+		WAVE sweep_number = IPNWB#H5_LoadDataset(groupID, "sweep_number")
+		WAVE/T series = IPNWB#H5_LoadDataset(groupID, "series")
+		series[] = (series[p])[2,inf] // Remove leading group linker "G:"
+		HDF5CloseGroup/Z groupID
+		return [sweep_number, series]
+	endif
+
+	return [$"", $""]
 End

IPNWB_Writer.ipf
@@ -154,7 +154,6 @@ threadsafe Function AddDevice(locationID, name, version, description)
 		H5_WriteTextDataset(groupID, path, str=description, skipIfExists=1)
 	elseif(version == NWB_VERSION_LATEST)
 		H5_CreateGroupsRecursively(locationID, path, groupID=groupID)
-		H5_WriteTextDataSet(groupID, "description", str=description, overwrite=1)
 		WriteBasicAttributes(groupID, path, "A recording device e.g. amplifier", "core", "Device")
 	endif
 
@@ -229,12 +228,15 @@ End
 ///
 /// @param locationID                                            HDF5 identifier
 /// @param fullAbsPath                                           absolute path to the TimeSeries dataset
+/// @param version                                               major NWB version
 /// @param unitWithPrefix                                        unit with optional prefix of the data in the TimeSeries, @see ParseUnit
 /// @param resolution [optional, defaults to `NaN` for unknown]  experimental resolution
 /// @param overwrite [optional, defaults to false] 				 should existing attributes be overwritten
-threadsafe Function AddTimeSeriesUnitAndRes(locationID, fullAbsPath, unitWithPrefix, [resolution, overwrite])
+threadsafe Function AddTimeSeriesUnitAndRes(locationID, fullAbsPath, version, unitWithPrefix, [resolution, overwrite])
 	variable locationID
-	string fullAbsPath, unitWithPrefix
+	string fullAbsPath
+	variable version
+	string unitWithPrefix
 	variable resolution, overwrite
 
 	string prefix, unit
@@ -253,9 +255,11 @@ threadsafe Function AddTimeSeriesUnitAndRes(locationID, fullAbsPath, unitWithPre
 		ParseUnit(unitWithPrefix, prefix, numPrefix, unit)
 	endif
 
-	H5_WriteTextAttribute(locationID, "unit"      , fullAbsPath, str=unit)
-	H5_WriteAttribute(locationID    , "conversion", fullAbsPath, numPrefix, IGOR_TYPE_32BIT_FLOAT)
-	H5_WriteAttribute(locationID    , "resolution", fullAbsPath, resolution, IGOR_TYPE_32BIT_FLOAT)
+	H5_WriteTextAttribute(locationID, "unit", fullAbsPath, str=unit)
+	if(version == 1)
+		H5_WriteAttribute(locationID, "conversion", fullAbsPath, numPrefix, IGOR_TYPE_32BIT_FLOAT)
+		H5_WriteAttribute(locationID, "resolution", fullAbsPath, resolution, IGOR_TYPE_32BIT_FLOAT)
+	endif
 End
 
 /// @brief Add a TimeSeries property to the `names` and `data` waves and removes it from `missing_fields` list
@@ -386,7 +390,6 @@ threadsafe Function WriteSingleChannel(locationID, path, version, p, tsp, [compr
 	endif
 
 	H5_CreateGroupsRecursively(locationID, group, groupID=groupID)
-	H5_WriteTextAttribute(groupID, "description", group, str=PLACEHOLDER, overwrite=1)
 
 	// write source attribute
 	if(version == 1)
@@ -403,7 +406,6 @@ threadsafe Function WriteSingleChannel(locationID, path, version, p, tsp, [compr
 		endif
 		H5_WriteTextAttribute(groupID, "source", group, str=source, overwrite=1)
 	elseif(version == NWB_VERSION_LATEST)
-		H5_WriteAttribute(groupID, "sweep_number", group, p.sweep, IGOR_TYPE_32BIT_INT | IGOR_TYPE_UNSIGNED, overwrite=1)
 		AppendToSweepTable(locationID, group, p.sweep)
 	endif
 
@@ -411,8 +413,6 @@ threadsafe Function WriteSingleChannel(locationID, path, version, p, tsp, [compr
 	if(p.channelType != CHANNEL_TYPE_OTHER)
 		if(version == 1)
 			H5_WriteTextAttribute(groupID, "comment", group, str=note(p.data), overwrite=1)
-		elseif(version == NWB_VERSION_LATEST)
-			H5_WriteTextAttribute(groupID, "comments", group, str=note(p.data), overwrite=1)
 		endif
 	endif
 
@@ -451,7 +451,7 @@ threadsafe Function WriteSingleChannel(locationID, path, version, p, tsp, [compr
 
 	// TimeSeries: datasets and attributes
 	// no timestamps, control, control_description and sync
-	AddTimeSeriesUnitAndRes(groupID, group + "/data", WaveUnits(p.data, -1), overwrite=1)
+	AddTimeSeriesUnitAndRes(groupID, group + "/data", version, WaveUnits(p.data, -1), overwrite=1)
 	if(version == 1)
 		H5_WriteDataset(groupID, "num_samples", var=DimSize(p.data, ROWS), varType=IGOR_TYPE_32BIT_INT, overwrite=1)
 	endif
@@ -572,12 +572,12 @@ threadsafe static Function AppendToSweepTable(locationID, reference, sweepNumber
 	STRUCT VectorIndex series_index
 	InitVectorIndex(series_index)
 	series_index.target = series
-	WriteBasicAttributes(groupID, "series_index", series_index.help, series_index.namespace, series_index.neurodata_type)
+	WriteBasicAttributes(groupID, "series_index", "", series_index.namespace, series_index.neurodata_type)
 	H5_WriteTextAttribute(groupID, "target", "series_index", str = "D:" + series_index.target.path, refMode = OBJECT_REFERENCE)
 
 	STRUCT VectorData sweep_number
 	InitVectorData(sweep_number)
-	sweep_number.description = "Sweep number of the entries in that row"
+	sweep_number.description = "The sweep number of the PatchClampSeries in that row"
 	WriteBasicAttributes(groupID, "sweep_number", sweep_number.help, sweep_number.namespace, sweep_number.neurodata_type)
 	H5_WriteTextAttribute(groupID, "description", "sweep_number", str = sweep_number.description)
 

doc/nwb.base.yaml
@@ -35,10 +35,6 @@ datasets:
       reftype: object
       target_type: VectorData
     name: target
-  - doc: a help string
-    dtype: text
-    name: help
-    value: indexes into a list of values for a list of elements
   doc: Pointers that index data values
   neurodata_type_def: VectorIndex
   neurodata_type_inc: Index
@@ -124,20 +120,6 @@ groups:
     shape:
     - null
   - attributes:
-    - default_value: 1
-      doc: Scalar to multiply each element in data to convert it to the specified
-        unit
-      dtype: float32
-      name: conversion
-      required: false
-    - default_value: 0
-      doc: 'Smallest meaningful difference between values in data, stored in the specified
-        by unit. COMMENT: E.g., the change in value of the least significant bit,
-        or a larger number if signal noise is known to be present. If unknown, use
-        -1.0'
-      dtype: float32
-      name: resolution
-      required: false
     - doc: 'The base unit of measure used to store data. This should be in the SI
         unit. COMMENT: This is the SI unit (when appropriate) of the stored data,
         such as Volts. If the actual data is stored in millivolts, the field ''conversion''

doc/nwb.file.yaml
@@ -8,7 +8,7 @@ groups:
       format with trailing major, minor and patch numbers.'
     dtype: text
     name: nwb_version
-    value: 2.0b
+    value: 2.0.1
   datasets:
   - dims:
     - '*unlimited*'

doc/nwb.icephys.yaml
@@ -7,45 +7,8 @@ groups:
   - doc: the protocol/stimulus name for this patch-clamp dataset
     dtype: text
     name: stimulus_description
-  - doc: Sweep number, allows to group different PatchClampSeries together.
-    dtype: uint
-    name: sweep_number
-    required: false
-  - default_value: no comments
-    doc: Human-readable comments about the TimeSeries. This second descriptive field
-      can be used to store additional information, or descriptive information if the
-      primary description field is populated with a computer-readable string.
-    dtype: text
-    name: comments
-    required: false
-  - default_value: no description
-    doc: Description of TimeSeries
-    dtype: text
-    name: description
-    required: false
   datasets:
-  - attributes:
-    - default_value: 1
-      doc: Scalar to multiply each element in data to convert it to the specified
-        unit
-      dtype: float32
-      name: conversion
-      required: false
-    - default_value: 0
-      doc: 'Smallest meaningful difference between values in data, stored in the specified
-        by unit. COMMENT: E.g., the change in value of the least significant bit,
-        or a larger number if signal noise is known to be present. If unknown, use
-        -1.0'
-      dtype: float32
-      name: resolution
-      required: false
-    - doc: 'The base unit of measure used to store data. This should be in the SI
-        unit. COMMENT: This is the SI unit (when appropriate) of the stored data,
-        such as Volts. If the actual data is stored in millivolts, the field ''conversion''
-        below describes how to convert the data to the specified SI unit.'
-      dtype: text
-      name: unit
-    dims:
+  - dims:
     - num_times
     doc: Recorded voltage or current.
     dtype: numeric
@@ -70,25 +33,6 @@ groups:
     dtype: text
     name: help
     value: Voltage recorded from cell during current-clamp recording
-  - doc: the protocol/stimulus name for this patch-clamp dataset
-    dtype: text
-    name: stimulus_description
-  - doc: Sweep number, allows to group different PatchClampSeries together.
-    dtype: uint
-    name: sweep_number
-    required: false
-  - default_value: no comments
-    doc: Human-readable comments about the TimeSeries. This second descriptive field
-      can be used to store additional information, or descriptive information if the
-      primary description field is populated with a computer-readable string.
-    dtype: text
-    name: comments
-    required: false
-  - default_value: no description
-    doc: Description of TimeSeries
-    dtype: text
-    name: description
-    required: false
   datasets:
   - doc: 'Unit: Amp'
     dtype: float32
@@ -114,25 +58,6 @@ groups:
     name: help
     value: Voltage from intracellular recordings when all current and amplifier settings
       are off
-  - doc: the protocol/stimulus name for this patch-clamp dataset
-    dtype: text
-    name: stimulus_description
-  - doc: Sweep number, allows to group different PatchClampSeries together.
-    dtype: uint
-    name: sweep_number
-    required: false
-  - default_value: no comments
-    doc: Human-readable comments about the TimeSeries. This second descriptive field
-      can be used to store additional information, or descriptive information if the
-      primary description field is populated with a computer-readable string.
-    dtype: text
-    name: comments
-    required: false
-  - default_value: no description
-    doc: Description of TimeSeries
-    dtype: text
-    name: description
-    required: false
   doc: Stores recorded voltage data from intracellular recordings when all current
     and amplifier settings are off (i.e., CurrentClampSeries fields will be zero).
     There is no CurrentClampStimulusSeries associated with an IZero series because
@@ -144,25 +69,6 @@ groups:
     dtype: text
     name: help
     value: Stimulus current applied during current clamp recording
-  - doc: the protocol/stimulus name for this patch-clamp dataset
-    dtype: text
-    name: stimulus_description
-  - doc: Sweep number, allows to group different PatchClampSeries together.
-    dtype: uint
-    name: sweep_number
-    required: false
-  - default_value: no comments
-    doc: Human-readable comments about the TimeSeries. This second descriptive field
-      can be used to store additional information, or descriptive information if the
-      primary description field is populated with a computer-readable string.
-    dtype: text
-    name: comments
-    required: false
-  - default_value: no description
-    doc: Description of TimeSeries
-    dtype: text
-    name: description
-    required: false
   doc: Aliases to standard PatchClampSeries. Its functionality is to better tag PatchClampSeries
     for machine (and human) readability of the file.
   neurodata_type_def: CurrentClampStimulusSeries
@@ -172,25 +78,6 @@ groups:
     dtype: text
     name: help
     value: Current recorded from cell during voltage-clamp recording
-  - doc: the protocol/stimulus name for this patch-clamp dataset
-    dtype: text
-    name: stimulus_description
-  - doc: Sweep number, allows to group different PatchClampSeries together.
-    dtype: uint
-    name: sweep_number
-    required: false
-  - default_value: no comments
-    doc: Human-readable comments about the TimeSeries. This second descriptive field
-      can be used to store additional information, or descriptive information if the
-      primary description field is populated with a computer-readable string.
-    dtype: text
-    name: comments
-    required: false
-  - default_value: no description
-    doc: Description of TimeSeries
-    dtype: text
-    name: description
-    required: false
   datasets:
   - attributes: []
     doc: 'Unit: Farad'
@@ -237,25 +124,6 @@ groups:
     dtype: text
     name: help
     value: Stimulus voltage applied during voltage clamp recording
-  - doc: the protocol/stimulus name for this patch-clamp dataset
-    dtype: text
-    name: stimulus_description
-  - doc: Sweep number, allows to group different PatchClampSeries together.
-    dtype: uint
-    name: sweep_number
-    required: false
-  - default_value: no comments
-    doc: Human-readable comments about the TimeSeries. This second descriptive field
-      can be used to store additional information, or descriptive information if the
-      primary description field is populated with a computer-readable string.
-    dtype: text
-    name: comments
-    required: false
-  - default_value: no description
-    doc: Description of TimeSeries
-    dtype: text
-    name: description
-    required: false
   doc: Aliases to standard PatchClampSeries. Its functionality is to better tag PatchClampSeries
     for machine (and human) readability of the file.
   neurodata_type_def: VoltageClampStimulusSeries

commit 4610c62ac01df25e21c18add0f9b004abf65cdcc

InitSubjectInfo: Use PLACEHOLDER for all values
The current values for description and date_of_birth don't make any
sense.

Bug introduced in bfdb939c (Neurodata Without Borders 2.0.1,
2020-01-09).

IPNWB_Structures.ipf
@@ -113,14 +113,14 @@ EndStructure
 threadsafe Function InitSubjectInfo(si)
 	STRUCT SubjectInfo &si
 
-	si.age         = PLACEHOLDER
-	si.date_of_birth = GetISO8601TimeStamp()
-	si.description = "Description of subject and where subject came from (e.g., breeder, if animal)."
-	si.genotype    = PLACEHOLDER
-	si.sex         = PLACEHOLDER
-	si.species     = PLACEHOLDER
-	si.subject_id  = PLACEHOLDER
-	si.weight      = PLACEHOLDER
+	si.age           = PLACEHOLDER
+	si.date_of_birth = PLACEHOLDER
+	si.description   = PLACEHOLDER
+	si.genotype      = PLACEHOLDER
+	si.sex           = PLACEHOLDER
+	si.species       = PLACEHOLDER
+	si.subject_id    = PLACEHOLDER
+	si.weight        = PLACEHOLDER
 End
 
 /// @brief Structure to hold all properties of the NWB file directly below `/`